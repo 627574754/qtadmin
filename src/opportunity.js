a1e6823c522d545bd2c9cd277572ec2965c748c0
lmj <627574754@qq.com>
2015-08-29 15:25:49 +0800

    dialog

--- src/opportunity.js
+++ src/opportunity.js
@@ -104,4 +104,17 @@
 	fileUpload();
 	//transaction 列表加载
 	loadTstTable();
+
+    var dialog = new gl.Dialog({
+        title: '系统提示', //窗口标题的html，如果不设置则无标题
+        content: '<div class="ui-dialog-bd"><p>欢迎！</p></div>',
+        //窗口内容的html，必须是html格式不能是无格式纯文本，如果不设置则无内容
+        beforeClose: null, //调用close方法时执行的callback，如果此callback返回false则会阻止窗口的关闭
+        showClose: false,
+        showFooter: true,
+        className: '', //窗口最外层容器的类名
+        cache: true, //是否缓存。若为false则close()的时候会remove掉对话框对应的dom元素
+        width: '40%' //窗口宽度，如不传递默认为40%
+    });
+    dialog.open();
 })();
