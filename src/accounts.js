c9b688f29a6303c2cb9234306c966ceda2fb60a5
张于栖 <zhangyuqi@zhangyuqideMacBook-Pro.local>
2015-09-10 11:27:40 +0800

    update

--- src/accounts.js
+++ src/accounts.js
@@ -1,8 +1,13 @@
 (function(){
 	var gl = window.gl;
 
+	var iptDlgs = {};
+
 	//初始化页面
 	function initHandle() {
+		//初始化下拉框
+		gl.initSelect();
+
 		//底部初始化
 		var handle = $('#btmHandle');
 		var group = $('#btmGroup');
@@ -27,9 +32,10 @@
 			curPage.animate({left: '0px'}, 300);
 		})
 		//返回列表页
-		var backBtn = $('#backList');
-		backBtn.on('click', function() {
-			var curPage = pages.eq(0);  //列表页
+		var jumpBtn = $('.jumpBtn');
+		jumpBtn.on('click', function() {
+			var i = $(this).data('pg');
+			var curPage = pages.eq(i);  //返回页面
 			setTimeout(function(){
 				pages.not(curPage).css('left','200%');
 				curPage.removeClass('moving');
@@ -38,11 +44,42 @@
 			curPage.animate({left: '0px'}, 300);
 		});
 	}
+	//初始化输入框组
+	function initIptGroup() {
+		var iptGroups = $('.iptGroup');
+		iptGroups.on('click', function(){
+			var name = $(this).data('name');
+			iptDlgs[name].open();
+		});
+		for (var i = iptGroups.length - 1; i >= 0; i--) {
+			var el = $(iptGroups[i]);
+			var name = el.data('name');
+			var dlg = $('#' + name + 'Dlg');
+			var title = el.data('title');
+			var tmp = dlg.html();
+			iptDlgs[name] = new gl.Dialog({
+				title: title, 
+				content: tmp,
+				width: '40%' //窗口宽度，如不传递默认为40%
+			});
+			var cnt = $('.ui-dialog-pop .'+name +'Content');
+			$('.items', cnt).on('click', function(){
+				var n = $(this).parents('.dl-content').data('name');
+				var ipt = $('input[name="'+n+'"]');
+				var val = $(this).data('value');
+				iptDlgs[n].close();
+				ipt.val(val);
+			});
+		};
+	}
+	
 	
 	//初始化页面
 	initHandle();
 	//页面切换
 	pageSwitch();
+	//初始化input框
+	initIptGroup();
 })();
 
 
